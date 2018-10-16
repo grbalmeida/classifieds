0cc0293d845d4f21e0ecf2a946c3b02b1e05f8e8
gilvan <g.r.almeida@live.com>
2018-10-16 00:51:35 -0300

    Criação da estrutura inicial da página inicial

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php require_once 'componentes/header.php'; ?>
+	<div class="container-fluid mt-3">
+		<div class="jumbotron">
+			<h3>Nós temos hoje 999 anúncios.</h3>
+			<p>E mais de 999 usuários cadastrados.</p>
+		</div>
+		<div class="row">
+			<div class="col-sm-3">
+				<h4>Filtros</h4>
+			</div>
+			<div class="col-sm-9">
+				<h4>Últimos Anúncios</h4>
+			</div>
+		</div>
+	</div>
+<?php require_once 'componentes/footer.php'; ?>
