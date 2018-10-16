941ceab91e64ab9cab848b2aea9c6a562c95cde2
gilvan <g.r.almeida@live.com>
2018-10-16 00:20:58 -0300

    Criação da tabela de usuários, categorias e anúncios

---
+++ database/banco.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE classificados;
+
+USE classificados;
+
+CREATE TABLE usuarios(
+    id int primary key auto_increment,
+    nome varchar(100) not null,
+    email varchar(100) not null,
+    senha varchar(32) not null,
+    telefone varchar(11)
+);
+
+CREATE TABLE categorias(
+    id int primary key auto_increment,
+    nome varchar(100) not null
+);
+
+CREATE TABLE anuncios(
+    id int primary key auto_increment,
+    id_usuario int not null,
+    id_categoria int not null,
+    titulo varchar(150) not null,
+    descricao text,
+    valor decimal(10, 2) not null,
+    estado enum('1', '2', '3') not null,
+    foreign key(id_usuario) references usuarios(id),
+    foreign key(id_categoria) references categorias(id)
+);
