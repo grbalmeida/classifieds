895037940e8502be0b317e4fd4810b0a013d0ee5
gilvan <g.r.almeida@live.com>
2018-10-16 02:48:51 -0300

    Utilizando componente de email e senha

--- cadastre-se.php
+++ cadastre-se.php
@@ -48,14 +48,7 @@
 			<label for="nome">Nome</label>
 			<input type="text" name="nome" id="nome" class="form-control">
 		</div>
-		<div class="form-group">
-			<label for="email">E-mail</label>
-			<input type="email" name="email" id="email" class="form-control">
-		</div>
-		<div class="form-group">
-			<label for="senha">Senha</label>
-			<input type="password" name="senha" id="senha" class="form-control">
-		</div>
+		<?php require_once 'componentes/email-senha.php'; ?>
 		<div class="form-group">
 			<label for="telefone">Telefone</label>
 			<input type="text" name="telefone" id="telefone" class="form-control">
