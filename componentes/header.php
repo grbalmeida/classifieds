5b13fb02e13208bd5f55c424c637ec3482f811cc
gilvan <g.r.almeida@live.com>
2018-10-16 00:50:39 -0300

    Criação dos componentes footer e header

---
+++ componentes/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>Classicados</title>
+	<link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<script src="node_modules/jquery/dist/jquery.min.js"></script>
+	<script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<nav class="navbar navbar-dark bg-dark">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a href="/" class="navbar-brand">Classificados</a>
+			</div>
+			<div>
+				<ul class="nav navbar-nav navbar-right">
+					<li>
+						<a href="" class="text-white">Cadastre-se</a> | 
+						<a href="" class="text-white">Login</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
